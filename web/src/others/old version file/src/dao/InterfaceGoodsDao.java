c507d2e0e43205c9ff7173b5e4f6463c4c950eed
Atreus1125 <73338696+Atreus1125@users.noreply.github.com>
2022-10-05 21:59:43 +0800

    Delete web/src/others/old version file directory

--- web/src/others/old version file/src/dao/InterfaceGoodsDao.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import java.util.HashMap;
-
-import entity.Goods;
-
-public interface InterfaceGoodsDao {
-	public abstract Goods findGoods(String id);
-}
--- web/src/others/old version file/src/service/InterfaceGoodsService.java
+++
@@ -1,7 +0,0 @@
-package service;
-
-import entity.Goods;
-
-public interface InterfaceGoodsService {
-	public abstract Goods findGoods(String id);
-}
